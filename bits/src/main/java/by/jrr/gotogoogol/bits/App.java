97c994f2fcb12688f94f34ffd716ba2f2c88c81a
max-java <6666350@gmail.com>
2020-08-09 17:03:20 +0300

    base setup, initial commit

---
+++ bits/src/main/java/by/jrr/gotogoogol/bits/App.java
@@ -0,0 +1,11 @@
+package by.jrr.gotogoogol.bits;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    public static Logger log = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        App.log.info("Hello World");
+    }
+}
---
+++ bits/src/test/java/by/jrr/gotogoogol/bits/AppTest.java
@@ -0,0 +1,12 @@
+package by.jrr.gotogoogol.bits;
+
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void main() {
+        App.log.info("Test is working");
+    }
+}
